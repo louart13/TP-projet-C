a1ff7c6512bca02c08caf01fdf97ea3b2db0b126
PPPPPL <67387603+PPPPLI@users.noreply.github.com>
2022-11-12 17:00:24 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,10 @@
 
 
 int main(){
-
+    
+    //classifier les differents types de mot par le 3ème mot d'indication
+    //EX:  Nom:Mas+SG/Ver:IImp+PL+P3/Adj:Mas+SG/Adv
+       
     t_n_tree v_tree = fileImportation('r');
     t_n_tree n_tree = fileImportation('m');
     t_n_tree adj_tree = fileImportation('j');
