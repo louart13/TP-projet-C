8036464da4fb1297932931cbe20a12087aa4e5ef
PPPPPL <67387603+PPPPLI@users.noreply.github.com>
2022-11-13 10:48:36 +0100

    Add files via upload

---
+++ wordFind.c
@@ -0,0 +1,28 @@
+//
+// Created by 31799 on 13/11/2022.
+//
+#include "wordFind.h"
+#include <stdlib.h>
+
+p_node_base wordFind (p_node_base base,int seed){
+
+
+
+    if(base->ref->amount != 0){
+
+        return base;
+    }
+    p_node_base temp = NULL;
+    if(base->son[seed] != NULL){
+
+        temp = wordFind(base->son[seed],rand() % 2);
+        if(temp != NULL){
+            return temp;
+        }
+    }else if(base->son[seed] == NULL){
+
+        temp = wordFind(base,rand() % 2);
+    }
+
+    return temp;
+}
---
+++ wordFind.h
@@ -0,0 +1,10 @@
+//
+// Created by 31799 on 13/11/2022.
+//
+#include <stdio.h>
+#include "dicArbre.h"
+#ifndef PROJETC_WORDFIND_H
+#define PROJETC_WORDFIND_H
+
+p_node_base wordFind (p_node_base,int);
+#endif //PROJETC_WORDFIND_H
